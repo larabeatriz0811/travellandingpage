205211abb210ae84f3110517e52f84ef69a730d4
mary0800 <laragattinha7@gmail.com>
2023-10-03 15:22:31 -0300

    adiciona titulo e subtitulo

--- app/layout.js
+++ app/layout.js
@@ -6,47 +6,63 @@ export const metadata = {
   title: 'Travel Landing Page',
 }
 
-function Logo({x, y}) {
-return(
-  <p style={{left: x, top: y}} className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
+function Logo({ x, y }) {
+  return (
+    <p style={{ left: x, top: y }} className={styles.logo}>Ready<span className={styles.comercial}>&</span>Go</p>
   );
 }
 
-function ItemMenu({nome}) {
+function ItemMenu({ nome }) {
   return (
     <p className={styles.itemMenu}>{nome}</p>
   );
 }
 
-function Menu({children, x, y}) {
+function Menu({ children, x, y }) {
   return (
-    <ul style={{left: x, top:y}} className={styles.menu}>
+    <ul style={{ left: x, top: y }} className={styles.menu}>
       {children.map((itemMenu) => (
-       <li key={itemMenu.nome} > {itemMenu}</li>
+        <li key={itemMenu.nome} > {itemMenu}</li>
 
       ))}
     </ul>
   );
-  
+
+}
+
+function Titulo({ x, y }) {
+  return (
+    <h1 style={{ left: x, top: y }} className={styles.titulo}> Your perfect
+      honeymoon </h1>
+  );
+}
+
+function Subtitulo({ x, y }) {
+  return (
+    <h2 style={{ left: x, top: y }} className={styles.subtitulo}>
+      Mayami is more than a honeymoon destination – it’s a perfect romantic spot for couples who like  spending time in beach.  </h2>
+  );
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body><header>
-        <Logo x={135} y={53}/>
-      <Menu x={435} y={60}>
-        <ItemMenu nome="Home"/>
-        <ItemMenu nome="About"/>
-        <ItemMenu nome="Contact"/>
-        <ItemMenu nome="Blog"/>
-        <ItemMenu nome="Videos"/>
-      </Menu>
-        </header>
+        <Logo x={135} y={53} />
+        <Menu x={435} y={60}>
+          <ItemMenu nome="Home" />
+          <ItemMenu nome="About" />
+          <ItemMenu nome="Contact" />
+          <ItemMenu nome="Blog" />
+          <ItemMenu nome="Videos" />
+        </Menu>
+      </header>
         <main>
+          <Titulo x={135} y={238} />
+          <Subtitulo x={136} y={465.04} />
           {children}
-          </main>
-          </body>
+        </main>
+      </body>
     </html>
   )
 }
